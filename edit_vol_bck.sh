b3425c330149fb3067b40aae630b9f981fc45f08
ole <ole@shortcutoslo.no>
2022-12-07 17:06:09 +0100

    First test without a volume_list.txt

--- edit_vol_bck.sh
+++ edit_vol_bck.sh
@@ -1,15 +1,15 @@
 #!/bin/bash   
 
-## Version 1.3.1
+## Version 1.4.1
 
 ## Common variables
-SPEED=450000
 LOGDIR=/Users/ole/scripts/logs
+LOGF=$LOGDIR/edit_vol_bck_$TODAY.log
 TODAY="$(date '+%y%m%d_%H%M')"
 STODAY="$(date '+%y%m%d')"
-LOGF=$LOGDIR/edit_vol_bck_$TODAY.log
-EMAIL_ADRESS=ole@shortcutoslo.no
 EXCLUDE_LIST=/Users/ole/projects/git/scn_editvol_bck/edit_rsync_exclude.txt
+EMAIL_ADRESS=ole@shortcutoslo.no
+DEST=/Volumes/temp/scn_backup
 
 ## Script it baby!
 
@@ -18,23 +18,23 @@ echo "" >> $LOGF
 
 echo "" >> $LOGF
 echo "List of volumes to be backed up" >> $LOGF
-cat /Users/ole/projects/git/scn_editvol_bck/edit_vol_list.txt >> $LOGF
+eval "$VOL" >> $LOGF #List
 echo "" >> $LOGF
 
 ## Create destination folder
-mkdir -p /Volumes/temp/scn_backup/$VOL
+mkdir -p $DEST/$VOL
 
 ## The actual backup
-VOLS=`cat /Users/ole/projects/git/scn_editvol_bck/edit_vol_list.txt` #List of volumes to back up from edit_vol_list.txt
+VOLS=`mount | grep "_edit" | awk '{print substr($3, 10)}'` #List of volumes to back up from edit_vol_list.txt
 for VOL in $VOLS; do
 	echo "" >> $LOGF
 	echo "backup of $VOL starts now..." >> $LOGF
 
 	# Create destination folder 
-	mkdir -p /Volumes/temp/scn_backup/$VOL
+	mkdir -p $DEST/$VOL
 
 	# tar it off Facilis
-	tar --exclude="*.lck" --exclude="*.prlock" --exclude="*.pat" --exclude="*.awf" --exclude="*.zip" --exclude="*.mp4" --exclude="*.mov" --exclude="*.wav" --exclude="*.cfa" --exclude="*/SearchData/" --exclude="*/WaveformCache/"  -czvf /Volumes/temp/scn_backup/$VOL/$STODAY"_"$VOL.tar -C "/Volumes/$VOL/editorial/project/" . >> $LOGF
+	tar --exclude="*.lck" --exclude="*.prlock" --exclude="*.pat" --exclude="*.awf" --exclude="*.zip" --exclude="*.mp4" --exclude="*.mov" --exclude="*.wav" --exclude="*.cfa" --exclude="*/SearchData/" --exclude="*/WaveformCache/"  -czvf $DEST/$VOL/$STODAY"_"$VOL.tar -C "/Volumes/$VOL/editorial/project/" . >> $LOGF
 
 	echo "" >> $LOGF
 
@@ -42,7 +42,7 @@ done
 
 ## Some sexy reports
  echo "Size of backups" >> $LOGF
-   du -shc /Volumes/temp/scn_backup/* | sort -n >> $LOGF
+   du -shc $DEST/* | sort -n >> $LOGF
    echo "" >> $LOGF
    echo "Size of volumes" >> $LOGF
    df -h | grep _edit >> $LOGF
