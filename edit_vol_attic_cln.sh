7aa68b2ee986fee0f90b8d06135f2478bb0476a7
ole <ole@shortcutoslo.no>
2022-12-08 11:22:39 +0100

    New and improved Attic cln

--- edit_vol_attic_cln.sh
+++ edit_vol_attic_cln.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
 ## Common variables
-SPEED=450000
 LOGDIR=/Users/ole/scripts/logs
 TODAY="$(date '+%y%m%d_%H%M')"
 STODAY="$(date '+%y%m%d')"
 LOGF=$LOGDIR/edit_vol_attic_cln_$TODAY.log
 EMAIL_ADRESS=ole@shortcutoslo.no
+VOLS=`mount | grep "_edit" | awk '{print substr($3, 10)}'` #This list backs up all network disks with the name _edit
+UNITYVOLS
 
+echo "Cleaning started on $HOSTNAME on $TODAY" >> $LOGF
+echo "" >> $LOGF
 
-VOLS=`cat /Users/ole/projects/git/scn_editvol_bck/edit_vol_unity_list.txt` #List of volumes to back up from edit_vol_list.txt
+echo "" >> $LOGF
+echo "List of volumes to be cleaned" >> $LOGF
+echo "$VOLS" >> $LOGF #List
+echo "" >> $LOGF
+
 for VOL in $VOLS; do
     echo "" >> $LOGF
     echo "Cleaning out Attics of $VOL..." >> $LOGF
 
-    # Create zip backup directory
-    mkdir -p /Volumes/$VOL/editorial/tmp/_unity-bak
-
     # Zip it
-    pushd /Volumes/$VOL/Unity\ Attic
-    zip -r -q -T -m -9 "$STODAY"_"$VOL".zip . >> $LOGF
-    popd
-
-    # Clean it out, moving it to a tmp-folder
-    pushd /Volumes/$VOL/editorial/tmp/_unity-bak
-    mv /Volumes/$VOL/Unity\ Attic/"$STODAY"_"$VOL".zip .
+    pushd /Volumes/$VOL/
+    zip -r -q -T -m Unity\ Attic/"$TODAY"_"$VOL"_attic.zip Unity\ Attic/* -x "*.zip" >> $LOGF
     popd
 
 done
+
+## Some sexy reports
+ echo "Size of backups" >> $LOGF
+   du -sh /Volumes/$VOLS/Unity\ Attic | sort -h >> $LOGF
+   echo "" >> $LOGF
+
+    echo "" >> $LOGF
+    echo "Cleaning is done... " >> $LOGF
+    cat $LOGF
+
+## Sending log to email recipients
+/opt/homebrew/bin/mutt -s "Cleaning $TODAY - log for edit disks" $EMAIL_ADRESS < $LOGF
