918c787d7bc72bd0b5044c6caebbf89fb434fcd3
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2025-05-09 11:28:26 +0200

    updated awk

--- edit_vol_bck.sh
+++ edit_vol_bck.sh
@@ -43,7 +43,7 @@ $MKDIR -p $LOGDIR/editvol_bck # this line creates the directory if it does not e
 LOGF=$LOGDIR/editvol_bck/edit_vol_bck_$TODAY.log
 EXCLUDE_LIST=~/git/editvol_bck/edit_exclude.txt
 EMAIL_ADRESS=scntech@shortcutoslo.no
-VOLS=$(mount | awk '$3 ~ /\/[a-z-]*_edit/ { print substr($3, 10)}') # this list backs up all network disks with the name _edit, and excluding those who dont.
+VOLS=$(mount | awk '$3 ~ /\/.*_edit/ { print substr($3, 10)}') # this list backs up all network disks with the name _edit
 
 ## Script it baby!
 echo "Backup started on $HOSTNAME on $TODAY" >> $LOGF
